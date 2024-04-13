4c51abf75b47d348a24bfd0c16e5c5f697622f70
Taehoon <kthsh98@gmail.com>
2024-04-12 19:47:24 -0600

    remove commented code

--- reactApp/src/attendance/users.jsx
+++ reactApp/src/attendance/users.jsx
@@ -30,25 +30,9 @@ export function Users(props) {
     };
   }, []);
 
-//   window.addEventListener('resize', hideUserDivOnMobile);
-
   function handleGameEvent(event) {
     setEvent([...events, event]);
   }
-
-//   function hideUserDivOnMobile() {
-//         const usersDiv = document.querySelector('.users');
-//         if (isMobile()) {
-//             usersDiv.style.display = 'none'; // 유저 디브 숨김
-//         } else {
-//             usersDiv.style.display = 'block'; // 유저 디브 보임
-//         }
-//     }
-    
-//     function isMobile() {
-//         return window.innerWidth <= 800; // 예시로 768px 이하를 모바일로 간주
-//     }
-    
 
   function createMessageArray() {
     const messageArray = [];
@@ -83,7 +67,6 @@ export function Users(props) {
   return (
     <div className={isMobile ? 'users hidden' : 'users visible'}>
         <div id='user-messages'>
-            {/* <div className="event"><span className="user-event">Welcome</span></div> */}
             {createMessageArray()}
         </div>
     </div>
